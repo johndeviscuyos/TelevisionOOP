a81c9b7a582e054653ca3a634f3ca2f01327ddbf
Devis <johndeviscuyos@gmail.com>
2024-05-22 16:02:19 +0800

    created a code for tv2 and added a code to print the result

--- Television.py
+++ Television.py
@@ -11,6 +11,10 @@ def main():
 #print the result for tv1
     print(f"tv1's channel is {tv1.get_channel()} and volume level is {tv1.get_volume()}")
 #create a code for tge required result for tv2
+    tv2 = TV()
+    tv2.turn_on()
+    tv2.set_volume(2)
+    tv2.set_channel(3)
 #print the result for tv2
-
+    print(f"tv2's channel is {tv2.get_channel()} and volume level is {tv2.get_volume()}")
 main()
