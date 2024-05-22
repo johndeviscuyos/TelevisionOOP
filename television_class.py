ef37f3b27111103e90dd420abd60f977c96127f5
Devis <johndeviscuyos@gmail.com>
2024-05-22 13:38:42 +0800

    added pseudocode

---
+++ television_class.py
@@ -0,0 +1,10 @@
+#create a constructor for channel, volume level and on
+#create a method for both turn on and off
+#crete a method for getting channel
+#create a method for setting channel and limit it to 1-120
+#create a method for getting volume
+#create a method for setting volume and limit it 1-7
+#create a method for increasing the channel
+#create a method for decreasing the channel
+#cheate a method for increasing volume
+#create a method for decreasing volume
