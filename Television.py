bebd6811123e46e17fe6045dbd73a6b8a2050684
Devis <johndeviscuyos@gmail.com>
2024-05-22 16:00:27 +0800

    printed the result for tv

--- Television.py
+++ Television.py
@@ -2,16 +2,15 @@ from television_class import TV
 
 #create objects required for the television class
 def main():
+    # create a code for the required result for tv1
+
     tv1 = TV()
     tv1.turn_on()
     tv1.set_volume(3)
     tv1.set_channel(30)
-
-
-
-
-
-#create a code for the required result for tv1
 #print the result for tv1
+    print(f"tv1's channel is {tv1.get_channel()} and volume level is {tv1.get_volume()}")
 #create a code for tge required result for tv2
 #print the result for tv2
+
+main()
