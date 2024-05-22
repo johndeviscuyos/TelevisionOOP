333f3964d980c4beefe3cb68236cf0f7e71dba24
Devis <johndeviscuyos@gmail.com>
2024-05-22 15:55:43 +0800

    created a code for the required output for tv1

--- Television.py
+++ Television.py
@@ -1,5 +1,16 @@
+from television_class import TV
 
 #create objects required for the television class
+def main():
+    tv1 = TV()
+    tv1.turn_on()
+    tv1.set_volume(3)
+    tv1.set_channel(30)
+
+
+
+
+
 #create a code for the required result for tv1
 #print the result for tv1
 #create a code for tge required result for tv2
