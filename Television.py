695e970a0fdf817fa1b7600f5f47aceba3dab1f9
Devis <johndeviscuyos@gmail.com>
2024-05-22 15:52:12 +0800

    created an object file and added pseudocodes

---
+++ Television.py
@@ -0,0 +1,6 @@
+
+#create objects required for the television class
+#create a code for the required result for tv1
+#print the result for tv1
+#create a code for tge required result for tv2
+#print the result for tv2
